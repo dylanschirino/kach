436f13e8264a0afc19f6c3f7a05925194a704b5b
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-10-21 10:36:02 +0200

    Implement reset-db gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@
 
 var gulp = require( "gulp" ),
     gESLint = require( "gulp-eslint" ),
-    gBabel = require( "gulp-babel" );
+    gBabel = require( "gulp-babel" ),
+    gUtil = require ( "gulp-util" ),
+    Mongo = require ( "mongodb" ),
+    ObjectID = Mongo.ObjectID,
+    MongoClient = Mongo.MongoClient;
 
 gulp.task( "lint", function() {
     return gulp
@@ -26,6 +30,70 @@ gulp.task( "build", function() {
         .src( "src/**/*.js" )
         .pipe( gBabel() )
         .pipe( gulp.dest( "bin" ) )
+} );
+
+gulp.task( "reset-db", function( fNext ){
+
+  // 1. Verify that we are INSIDE the vagrant donc si le nom d'utilisateur est différent de vagrant c'est qu'on est hors de la vagrant.
+
+  if ( process.env.USER !== "vagrant" ) {
+    gUtil.beep();
+    gUtil.log ( gUtil.colors.red( "This task must be runned from INSIDE the vagrant box") );
+    return fNext();
+  }
+  // Connect to mongodb
+  MongoClient.connect("mongodb://127.0.0.1:27017/kach", function( oError, oDB ){
+
+    var fDataParser;
+
+    if( oError ){
+      gUtil.beep();
+      return fNext( oError );
+    }
+
+    fDataParser = function( oElt ){
+      oElt._id = new ObjectID( oElt._id.$oid );
+
+      if ( oElt.bank && oElt.bank.$oid ) {
+        oElt.bank = new ObjectID( oElt.bank.$oid );
+      }
+
+      oElt.created_at = new Date( oElt.created_at );
+      oElt.updated_at = new Date( oElt.updated_at );
+      if ( oElt.deleted_at ) {
+        oElt.deleted_at = new Date( oElt.deleted_at );
+      }
+      return oElt;
+    };
+    // 2. Drop Databases
+
+    oDB.dropDatabase()
+      .then( function(){
+        // 3. Parse & Fill Banks
+        var aBanks = require( __dirname + "/_dev/banks.json" );
+        //Map : ca prend chaque élément dessus, on fait le traitement et ca nous retourne un nouveau tableau avec les valeurs modifié
+        return oDB.collection( "banks" ).insertMany( aBanks.map( fDataParser ) );
+
+      } )
+      .then( function(){
+        // 4. Parse & Fill terminals
+        var aTerminals = require( __dirname + "/_dev/terminals.json" );
+        return oDB.collection( "terminals" ).insertMany( aTerminals.map( fDataParser ) );
+
+      } )
+      .then( function() {
+        oDB.close();
+        gUtil.log( gUtil.colors.green( "DB has been reset ") );
+        fNext();
+      } )
+      .catch( function( oError ){
+
+        oDB.close();
+        fNext( oError );
+
+      } );
+  } );
+
 } );
 
 gulp.task( "watch", function() {
