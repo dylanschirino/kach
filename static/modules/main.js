d55d3f8748eef9e22ea247532ddf72d72d2d6863
Dylan Schirino <dylan.schirino@hotmail.fr>
2016-12-09 12:22:50 +0100

    Install dependecies and add base dummy modules

---
+++ static/modules/main.js
@@ -0,0 +1,9 @@
+/* Dylan/Kash
+ *
+ * /static/modules/main.js - Main Entry files
+ *
+ * Coded by Dylan Schirino
+ * started at 09/12/2016
+ */
+import utils from "./utils";
+console.log( "hey oh" );
---
+++ static/modules/utils.js
@@ -0,0 +1,10 @@
+/* Dylan/Kash
+ *
+ * /static/modules/utils.js - Small utils
+ *
+ * Coded by Dylan Schirino
+ * started at 09/12/2016
+ */
+export default function(){
+  console.log("hey from utils");
+}
